f1aff97f3ff611fb135f4c44c67ddae441e2c17d
Boyan Bonev <bonev123_1996@abv.bg>
2023-06-18 14:48:33 +0300

    Add files via upload

--- 4.React State in the Component Tree/App.js
+++ 4.React State in the Component Tree/App.js
@@ -1,1 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
 
+const [firstCity, second] = ["Tokyo", "London"]
+
+console.log(second)
+
+function App({library}) {
+  return (
+    <div className="App">
+        
+        <p>
+          Hello, {library}
+        </p>
+        </div>
+  );
+}
+
+export default App;
